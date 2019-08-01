1883e48d9d7c168bf883634cee32ba4692bd77b2
adumans <buaadjh@163.com>
2019-07-31 22:19:37 -0400

    bayes training trials

--- code/bayes_params_selection.py
+++ code/bayes_params_selection.py
@@ -6,7 +6,7 @@ import ExtractionFeatures
 from sklearn import model_selection
 import lightgbm as lgb
 from hyperopt import STATUS_OK
-from hyperopt import fmin,tpe,hp,partial
+from hyperopt import fmin,tpe,hp,partial,Trials
 from sklearn import datasets
 from lightgbm import LGBMClassifier
 from sklearn.utils import column_or_1d
@@ -41,7 +41,7 @@ def objective(params, n_folds=N_FOLDS):
     # params['subsample_for_bin'] = int(params['subsample_for_bin'])
     # clf = LGBMClassifier(n_estimators=500,**params)
     # score = model_selection.cross_val_score(clf, train_features, train_labels, cv=3, scoring='roc_auc')
-    cv_results = lgb.cv(params, train_set, nfold=n_folds, num_boost_round=10000, early_stopping_rounds = 100, metrics = 'auc')
+    cv_results = lgb.cv(params, train_set, nfold=n_folds, num_boost_round=1000, early_stopping_rounds = 100, metrics = 'auc')
     score = max(cv_results['auc-mean'])
     loss = 1 - score
 
@@ -49,25 +49,40 @@ def objective(params, n_folds=N_FOLDS):
     # writer = csv.writer(of_connection)
     # writer.writerow([loss, params])
     # of_connection.close()
+    print ('loss: ', loss)
+    print ('params: ',params)
     return {'loss': loss, 'params': params, 'status': STATUS_OK}
 
 # Define the search space
+#space = {
+#   'class_weight': hp.choice('class_weight', [None, 'balanced']),
+#   'num_leaves': hp.choice('num_leaves', np.arange(30, 150, dtype=int)),
+#   'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.2)),
+#   'subsample_for_bin': hp.choice('subsample_for_bin', np.arange(20000, 300000, 20000, dtype=int)),
+#   'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
+#   'min_data_in_leaf': hp.choice('min_data_in_leaf', np.arange(30, 150, dtype=int)),
+#   'reg_alpha': hp.uniform('reg_alpha', 0.0, 1.0),
+#   'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+#   'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0)
+#}
+
 space = {
-   'class_weight': hp.choice('class_weight', [None, 'balanced']),
+   #'class_weight': hp.choice('class_weight', [None, 'balanced']),
    'num_leaves': hp.choice('num_leaves', np.arange(30, 150, dtype=int)),
-   'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.2)),
-   'subsample_for_bin': hp.choice('subsample_for_bin', np.arange(20000, 300000, 20000, dtype=int)),
-   'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
-   'min_data_in_leaf': hp.choice('min_data_in_leaf', np.arange(30, 150, dtype=int)),
-   'reg_alpha': hp.uniform('reg_alpha', 0.0, 1.0),
-   'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
-   'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0)
+   #'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.2)),
+   #'subsample_for_bin': hp.choice('subsample_for_bin', np.arange(20000, 300000, 20000, dtype=int)),
+   #'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
+   #'min_data_in_leaf': hp.choice('min_data_in_leaf', np.arange(30, 150, dtype=int)),
+   #'reg_alpha': hp.uniform('reg_alpha', 0.0, 1.0),
+   #'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+   #'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0)
 }
 
-
 algo = partial(tpe.suggest,n_startup_jobs=10)
-MAX_EVALS = 500
-best = fmin(fn = objective, space = space, algo = algo, max_evals = MAX_EVALS)
+MAX_EVALS = 50
+trials = Trials()
+best = fmin(fn = objective, space = space, algo = algo, max_evals = MAX_EVALS, trials = trials)
 
 print (best)
-i = 0
+for trial in trials.trials[:2]:
+    print (trial)
